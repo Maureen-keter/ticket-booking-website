49528b10863420a0a970eb28abf9d3cf0ab57046
maureen <maureen.chelangat@student.moringaschool.com>
2024-02-29 17:53:13 +0300

    create a route to get all routes:

--- backend/app.py
+++ backend/app.py
@@ -131,7 +131,7 @@ class Events(Resource):
     
 class EventById(Resource):
     def get(self, id):
-        event=Event.query.filter_by(id=id).first().to_dict()
+        event=Event.query.get(id).to_dict()
         return make_response(jsonify(event), 200)
     def patch(self, id):
         event = Event.query.get(id)
@@ -149,6 +149,42 @@ class EventById(Resource):
         if not event:
             abort(404, detail = f'Event with {id=} does not exist')
         db.session.delete(event)
+        db.session.commit()  
+
+class Tickets(Resource):
+    def get(self):
+        events=[ticket.to_dict for ticket in Ticket.query.all()]
+        return make_response(jsonify(events), 200)
+
+class TicketById(Resource):
+    def get(self, id):
+        ticket=Ticket.query.get(id).to_dict()
+        return make_response(jsonify(ticket), 200)
+    
+    def post(self):
+        data=request.get_json()
+        new_ticket=Ticket(ticket_type=data['ticket_type'], user_id=data['user_id'], event_id=data['event_id'])
+        db.session.add(new_ticket)
+        db.session.commit()
+        return make_response(jsonify({"message":"new event created successfully"}), 200)
+    
+    
+    def patch(self, id):
+        ticket = Ticket.query.get(id)
+        if not ticket:
+            abort(404, detail = f'Ticket with {id=} does not exist')
+        data = request.get_json()
+        for key, value in data.items():
+            if value is None:
+                continue
+            setattr(ticket, key, value)
+        db.session.commit()
+        return ticket.to_dict()
+    def delete(self, id):
+        ticket= Ticket.query.get(id)
+        if not ticket:
+            abort(404, detail = f'Ticket with {id=} does not exist')
+        db.session.delete()
         db.session.commit()  
 
 
